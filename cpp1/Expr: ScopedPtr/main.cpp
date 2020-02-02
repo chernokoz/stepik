57cf266829ac91bd13a460f77f85a8556e4ff597
chernokoz <55104370+chernokoz@users.noreply.github.com>
2020-02-02 19:26:24 +0300

    Create main.cpp

---
+++ cpp1/Expr: ScopedPtr/main.cpp
@@ -0,0 +1,42 @@
+struct Expression;
+struct Number;
+struct BinaryOperation;
+
+struct ScopedPtr
+{
+    // реализуйте следующие методы:
+    //
+     explicit ScopedPtr(Expression *ptr = 0) {
+         ptr_ = ptr;
+     }
+     ~ScopedPtr() {
+         delete ptr_;
+     }
+     Expression* get() const {
+         return ptr_;
+     }
+     Expression* release() {
+         Expression * tmp = ptr_;
+         ptr_ = 0;
+         return tmp;
+     }
+     void reset(Expression *ptr = 0) {
+         Expression * tmp = ptr_;
+         ptr_ = ptr;
+         delete tmp;
+     }
+     Expression& operator*() const {
+         return *ptr_;
+     }
+     Expression* operator->() const {
+         return ptr_;
+     }
+
+
+private:
+    // запрещаем копирование ScopedPtr
+    ScopedPtr(const ScopedPtr&);
+    ScopedPtr& operator=(const ScopedPtr&);
+
+    Expression *ptr_;
+};
